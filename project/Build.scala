cd896d0105288abb7365dc93fcfe3474c1a33e96
tattyamm <entatsu.appli+github@gmail.com>
2012-10-10 23:45:28 +0900

    redisをやめてmemcachedに差し替え

--- project/Build.scala
+++ project/Build.scala
@@ -4,24 +4,23 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "TwitterProxy"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName = "TwitterProxy"
+  val appVersion = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
+  val appDependencies = Seq(
+    // Add your project dependencies here,
 
-      //redis追加
-      "com.typesafe" %% "play-plugins-redis" % "2.0.2"
+    //memcached
+    "com.github.mumoshu" %% "play2-memcached" % "0.2.1-SNAPSHOT"
+  )
 
-      //redis用にjedisを追加
-      //"Redis.clients" % "jedis" % "2.0.0"
-    )
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here
+    //memcached
+    resolvers += "Sonatype OSS Snapshots Repository" at "http://oss.sonatype.org/content/groups/public",
+    resolvers += "Spy Repository" at "http://files.couchbase.com/maven2" // required to resolve `spymemcached`, the plugin's dependency.
 
-      //redisの為にsedisを入れる為
-      //resolvers += "Sedis repository" at "http://guice-maven.googlecode.com/svn/trunk"
-    )
+  )
 
 }
